9393aaa3087f965dba400aa66d7d0c427150a05d
emil torlen <>
2020-03-01 23:26:43 +0100

    script works

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
 import json
+import os
+from subprocess import Popen, PIPE        
 
-with open('out.json') as f:
-    hits = json.load(f)
-    print(hits["MAIN.cache_hit"]["value"])
+# output = subprocess.Popen("docker exec -it dockervarnishnginx_varnish_1  varnishstat -j", shell=True,stdout=PIPE,stderr=PIPE)
+p = Popen("cat /Users/emiltorlen/Documents/GIT/dynatrace-varnish-container/out.json", shell=True,stdout=PIPE,stderr=PIPE)
+#print(output)
+stdout, stderr = p.communicate()
+
+print(stderr)
+# print(stdout)
+h=json.loads(stdout)
+print(h["MAIN.cache_hit"]["value"])
+
+# cmd = "docker exec -it dockervarnishnginx_varnish_1  varnishstat -j > /home/dtuser/out1.json"
+# os.system(cmd)
+# with open('/home/dtuser/out1.json') as f:
+#     hits = json.load(f)
+#     print(hits["MAIN.cache_hit"]["value"])
+
--- varnishcontainerpluginremote.py
+++ varnishcontainerpluginremote.py
@@ -3,32 +3,39 @@ import logging
 import socket
 import json
 import os
+from subprocess import Popen, PIPE
 
 logger = logging.getLogger(__name__)
+
+
 class VarnishContainerPluginRemote(RemoteBasePlugin):
-	def initialize(self, **kwargs):
-		logger.info("Config: %s", self.config)
-		self.containerId = self.config["containerid"]
-	def query(self, **kwargs):
-		# Create group - provide group id used to calculate unique entity id in dynatrace
-		#   and display name for UI presentation
-		group = self.topology_builder.create_group(identifier="Varnish",
-												   group_name="Varnishstats")
-		# Create device - provide device id used to calculate unique entity id in dynatrace
-		#   and display name for UI presentation
-		device = group.create_device(identifier="varnishcache", display_name="VarnishCache")
-		# device.add_endpoint(ip=self.address, port=self.port)
-		logger.info("Topology: group name=%s, device name=%s", group.name, device.name)
-		# Execute command
-		try:
-			cmd = "docker exec -it " + self.containerId + "  varnishstat -j > /tmp/out.json"
-			os.system(cmd)
-			with open('/tmp/out.json') as f:
-				data = json.load(f)
-				device.absolute("cachehit",value=data["MAIN.cache_hit"]["value"])
-				#device.state_metric(key="cachehit",value=data["MAIN.cache_hit"]["value"])
-		except Exception as e:
-			logger.error("Fatal error in main loop", exc_info=True)
-			device.absolute(key='cachehit', value=0)
-		finally:
-			logger.info("Done")
+    def initialize(self, **kwargs):
+        logger.info("Config: %s", self.config)
+        self.containerId = self.config["containerid"]
+
+    def query(self, **kwargs):
+        # Create group - provide group id used to calculate unique entity id in dynatrace
+        #   and display name for UI presentation
+        group = self.topology_builder.create_group(identifier="Varnish",
+                                                   group_name="Varnishstats")
+        # Create device - provide device id used to calculate unique entity id in dynatrace
+        #   and display name for UI presentation
+        device = group.create_device(identifier="varnishcache",
+                                     display_name="VarnishCache")
+        # device.add_endpoint(ip=self.address, port=self.port)
+        logger.info("Topology: group name=%s, device name=%s", group.name,
+                    device.name)
+        logger.info("Container %s", self.containerId)
+        # Execute command
+        try:
+            cmd = "docker exec -it " + self.containerId + "  varnishstat -j"
+            p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+            stdout, stderr = p.communicate()
+            logger.info("Error executing command=%s , err=%s", cmd, stderr)
+            data = json.loads(stdout)
+            device.absolute("cachehit", value=data["MAIN.cache_hit"]["value"])
+        except Exception as e:
+            logger.error("Fatal error in main loop", exc_info=True)
+            device.absolute(key='cachehit', value=0)
+        finally:
+            logger.info("Done")
